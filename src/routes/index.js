c34e44ca50b77681ff8d226d91c51b5474644fd3
Schmerb <mike.schmerbeck@gmail.com>
2018-01-14 19:27:11 -0500

    added error feedback for incorrect query params

--- src/routes/index.js
+++ src/routes/index.js
@@ -9,15 +9,14 @@ router.use(bodyParser.json());
 
 
 // controllers
-const mainController = require('controllers/mainController');
+const mainController      = require('controllers/mainController');
+const candidateController = require('controllers/candidateController');
 
 
 router.get('/', mainController.getIndex);
 
-router.get('/candidates/all', mainController.getAllCandidates);
-
-router.get('/candidates/house', mainController.getHouseCandidates);
-
-router.get('/candidates/senate', mainController.getSenateCandidates);
+router.get('/candidates/all',    candidateController.getAllCandidates);
+router.get('/candidates/house',  candidateController.getHouseCandidates);
+router.get('/candidates/senate', candidateController.getSenateCandidates);
 
 module.exports = router;
