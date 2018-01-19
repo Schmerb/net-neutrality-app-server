5fb3a82471217bbd9504b6b433694d0b829416a1
Schmerb <mike.schmerbeck@gmail.com>
2018-01-18 20:36:37 -0500

    retrying client url

--- server.js
+++ server.js
@@ -33,12 +33,12 @@ app.use(
   cors({
       origin: [  // Whitelist 
           CLIENT_ORIGIN,
-          'https://suspicious-franklin-d3316b.netlify.com'
+          'https://suspicious-franklin-d3316b.netlify.com/'
       ]
   })
 );
 
-C.O.R.S.
+// C.O.R.S.
 app.use(function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
