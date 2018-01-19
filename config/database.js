fc91db459093b5965033a20efa487da844e4ce6f
Schmerb <mike.schmerbeck@gmail.com>
2018-01-18 20:28:18 -0500

    added CLIENT_ORIGIN as env var

--- config/database.js
+++ config/database.js
@@ -3,4 +3,5 @@ exports.DATABASE_URL = process.env.DATABASE_URL ||
                        'mongodb://localhost/net-db';
 exports.TEST_DATABASE_URL = (process.env.TEST_DATABASE_URL ||
                             'mongodb://localhost/test-net-db');
+exports.CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || 'http://localhost:3000/';
 exports.PORT = process.env.PORT || 8080;
